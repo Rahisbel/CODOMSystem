966877ece4910bf4e24c2f36ba8e444c1dac6099
Rahisbel Herrera <ryhn236@gmail.com>
2019-09-29 10:38:35 -0400

    conexion a la base de datos, registro e inicio de sesión

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>CODOMSystem</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -64,36 +64,22 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+        <header>
+                <div class="links">
+                    <a href="{{route('iniciar')}}">Iniciar Sesion</a>
+                    <a href="{{route('registrar')}}">Registrarse</a>
                 </div>
-            @endif
+
+        </header>
+    
+        <div class="flex-center position-ref full-height">
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    CODOMSystem
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+          
             </div>
         </div>
     </body>
--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,25 @@
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
 
+//Route::get('','InicioController@index')->name('principal');
+
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('principal');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/iniciar','inicioS@inicio')->name('iniciar');
+
+Route::get('/registrar','registro@registro')->name('registrar');
+
+
+
+
+/*Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');*/
