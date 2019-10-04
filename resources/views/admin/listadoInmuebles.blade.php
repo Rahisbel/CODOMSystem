84255cf489e2c17a9214e7f960ad75733a2bcf54
Rahisbel Herrera <ryhn236@gmail.com>
2019-10-03 22:49:57 -0400

    arreglado tamaño de la tabla inmueble

--- resources/views/admin/listadoInmuebles.blade.php
+++ resources/views/admin/listadoInmuebles.blade.php
@@ -2,60 +2,63 @@
 
 @section('content')
 <div class="container">
+		<div class="row justify-content-center">
+			<div class="col-md-8">
+				<h4 align="center">Listado de Inmuebles</h4>
 
-	<h4 align="center">Listado de Inmuebles</h4>
+				@if($inmuebles->isEmpty())
+					<h4>No hay inmuebles</h4>
+					@else
+						<h5>Casas</h5>
 
-	@if($inmuebles->isEmpty())
-		<h4>No hay inmuebles</h4>
-		@else
-			<h5>Casas</h5>
-
-			<table class="table table-sm table-dark">
-				<thead>
-				<tr>
-					<th scope="col">#</th>
-					<th scope="col">Precio</th>
-					<th scope="col">Alicuota</th>
-				</tr>
-				</thead>
-				<tbody>
-					@foreach($inmuebles as $property)	
-					<tr>
-						@if($property->type->name == 'Casa')
-						<th scope="row">{{$property->id}}</th>
-						    <td>{{$property->precio}}</td>
-						    <td>{{$property->alicuota}}</td>
-						@endif	
-					</tr>	
-					@endforeach
-				</tbody>
-			</table>
+						<table class="table table-sm table-dark">
+							<thead>
+							<tr align="center" >
+								<th align="center"  scope="col">#</th>
+								<th align="center"  scope="col">Precio</th>
+								<th align="center"  scope="col">Alicuota</th>
+							</tr>
+							</thead>
+							<tbody>
+								@foreach($inmuebles as $property)	
+								<tr align="center"  >
+									@if($property->type->name == 'Casa')
+									<th align="center"  scope="row">{{$property->id}}</th>
+									    <td align="center"  >{{$property->precio}}</td>
+									    <td align="center" >{{$property->alicuota}}</td>
+									@endif	
+								</tr>	
+								@endforeach
+							</tbody>
+						</table>
 
 
-			<h5>Apartamentos</h5>
+						<h5>Apartamentos</h5>
 
-			<table class="table table-sm table-dark">
-				<thead>
-				<tr>
-					<th scope="col">#</th>
-					<th scope="col">Precio</th>
-					<th scope="col">Alicuota</th>
-				</tr>
-				</thead>
-				<tbody>
-					@foreach($inmuebles as $property)	
-					<tr>
-						@if($property->type->name == 'Apartamento')
-						<th scope="row">{{$property->id}}</th>
-						    <td>{{$property->precio}}</td>
-						    <td>{{$property->alicuota}}</td>
-						@endif	
-					</tr>	
-					@endforeach
-				</tbody>
-			</table>	
-	@endif
+						<table class="table table-sm table-dark">
+							<thead>
+							<tr align="center" >
+								<th align="center"  scope="col">#</th>
+								<th align="center"  scope="col">Precio</th>
+								<th align="center"  scope="col">Alicuota</th>
+							</tr>
+							</thead>
+							<tbody>
+								@foreach($inmuebles as $property)	
+								<tr align="center" >
+									@if($property->type->name == 'Apartamento')
+									<th align="center" scope="row">{{$property->id}}</th>
+									    <td align="center">{{$property->precio}}</td>
+									    <td align="center">{{$property->alicuota}}</td>
+									@endif	
+								</tr>	
+								@endforeach
+							</tbody>
+						</table>	
+				@endif
 
-	<a href="{{url('administrador')}}" type="submit" class="btn btn-primary">{{ __('Volver') }}</a>
-</div>
+				<a href="{{url('administrador')}}" type="submit" class="btn btn-primary">{{ __('Volver') }}</a>
+			</div>
+		</div>
+	</div>
 @endsection
