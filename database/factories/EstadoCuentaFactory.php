04785339befcce15c2860542f10636b5933a7b86
Jesus <chjesussao@gmail.com>
2019-10-09 17:42:54 -0400

    vistas

---
+++ database/factories/EstadoCuentaFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\estadoCuenta;
+use Faker\Generator as Faker;
+
+$factory->define(estadoCuenta::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
